0948aaffaeaf2187c086d14cb119af104590ff86
marmanu <marcelo.mandirola@tarmac.io>
2023-06-25 21:35:58 -0300

    terraform practico 2

---
+++ azure/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "mlObli" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "mlObliNetwork" {
+  name                = var.private_network_name
+  resource_group_name = azurerm_resource_group.mlObli.name
+  location            = azurerm_resource_group.mlObli.location
+  address_space       = var.private_ip_range
+}
+
+resource "azurerm_storage_account" "mlObliStorage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.mlObli.name
+  location                 = azurerm_resource_group.mlObli.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "mlObliStorageContainer" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.mlObliStorage.name
+  container_access_type = "private"
+}
---
+++ azure/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_location" {
+  default     = "westus"
+}
+
+variable "private_ip_range"{
+    default = ["10.0.0.0/16"]
+}
+
+variable "resource_group_name" {
+  default     = "mlObli"
+}
+
+variable "private_network_name"{
+    default = "mlObliNetwork"
+}
+
+variable "storage_account_name"{
+    default = "mloblistorage"
+}
+
+variable "storage_container_name"{
+    default = "mloblistoragecontainer"
+}

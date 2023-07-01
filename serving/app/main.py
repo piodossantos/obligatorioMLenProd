ef8b8d7995f89927e7d6c0cc97f4b5a44c88c178
marmanu <marcelo.mandirola@tarmac.io>
2023-07-01 20:32:16 -0300

    serve lavadora

--- serving/app/main.py
+++ serving/app/main.py
@@ -5,13 +5,35 @@ from keras.models import load_model
 import numpy as np
 from PIL import Image
 import uvicorn
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 print('STARTING APP')
 
 app = FastAPI()
 
 try:
-    model = load_model('model.h5')
+    model = load_model('models/model1.h5', custom_objects={
+      'recall_m': recall_m,
+      'precision_m': precision_m,
+      'f1_m': f1_m
+    })
 except Exception as e:
     print(f"Error loading model: {e}")
     model = None
